4018a9c28a513690f020581e27ed7830f0c562b0
rajutalukdar1 <lalutaluker@gmail.com>
2022-11-08 00:24:04 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import route from './components/Route/Route';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='max-w-6xl mx-auto'>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
